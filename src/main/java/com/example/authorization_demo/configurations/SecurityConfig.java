4d352b947d4f4b74fcbc80f1d7b49758e8375035
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-06-05 11:49:11 +0200

    remove useless code

--- src/main/java/com/example/authorization_demo/configurations/SecurityConfig.java
+++ src/main/java/com/example/authorization_demo/configurations/SecurityConfig.java
@@ -31,23 +31,14 @@ public class SecurityConfig {
     @Autowired
     private AccountDetailService accountDetailService;
 
-//    private final UserService userService;
-
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
-//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/accounts").permitAll()
-//                        .requestMatchers("/swagger-ui/**").permitAll()
-//                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
-//                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS));
-//                .authenticationProvider(authenticationProvider());
         return http.build();
     }
 
@@ -67,11 +58,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return accountDetailService;
     }
 
 }
